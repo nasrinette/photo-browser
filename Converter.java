98682bd4b8e8f42267c91428a45cfe449e764713
nasrinette <nsrvansrn@gmail.com>
2025-09-08 00:41:54 +0200

    menu bar

--- Converter.java
+++ Converter.java
@@ -2,10 +2,9 @@ import javax.swing.*;
 import javax.swing.*;
 import java.awt.*;
 public class Converter extends JFrame {
-    private JLabel resultLabel;
+    // private JLabel resultLabel;
     public Converter() {
-        super("Temperature Converter");
-        setupInputPanel();
+        super("Photo browser");
         // var tempPanel = new JPanel();
         // add(tempPanel, BorderLayout.CENTER);
 
@@ -15,50 +14,83 @@ public class Converter extends JFrame {
         // var input = new JTextField( 10);
         // tempPanel.add(input);
         // setPreferredSize(new Dimension(600, 600));
-setupButtonPanel();
-setupResultPanel();
-        pack();
 
+
+    setPreferredSize(new Dimension(1000, 800));
+  
+       var menuBar = new JMenuBar();
+       var fileMenu = new JMenu("File");
+       menuBar.add(fileMenu);
+
+      var viewMenu = new JMenu("View");
+       menuBar.add(viewMenu);
+
+       var importItem = new JMenuItem("Import");
+       var deleteItem = new JMenuItem("Delete");
+       var quitItem = new JMenuItem("Quit");
+
+       fileMenu.add(importItem);
+       fileMenu.add(deleteItem);
+       fileMenu.add(quitItem);
+
+       var viewRdItem = new JRadioButtonMenuItem("View photo");
+ 
+
+       var browseRdItem = new JRadioButtonMenuItem("Browser");
+       viewMenu.add(viewRdItem);
+       viewMenu.add(browseRdItem);
+
+       
+       var viewGroup = new ButtonGroup();
+       viewGroup.add(viewRdItem);
+       viewGroup.add(browseRdItem);
+
+
+       setJMenuBar(menuBar);
+       var label = new JLabel("Main panel");
+      add(label);
+      pack();
+    //   setVisible(true);
 
 
 
 }
 
-private void setupInputPanel(){
-     var tempPanel = new JPanel();
-        add(tempPanel, BorderLayout.NORTH);
+// private void setupInputPanel(){
+//      var tempPanel = new JPanel();
+//         add(tempPanel, BorderLayout.NORTH);
 
-        var tempLabel = new JLabel("Temperature");
-        tempPanel.add(tempLabel);
+//         var tempLabel = new JLabel("Temperature");
+//         tempPanel.add(tempLabel);
 
-        var input = new JTextField( 10);
-        tempPanel.add(input);
-        setPreferredSize(new Dimension(600, 600));
-        // pack();
-
-
-    }
-private void setupButtonPanel(){
-    var btnPanel = new JPanel();
-    var convertToF = new JButton("Convert to F");   btnPanel.add(convertToF);
-convertToF.addActionListener(e->{
-    System.out.println("Convert to F button clicked");
-    resultLabel.setText("Result: 212 F");
-});
-    var convertToC = new JButton("Convert to C");    btnPanel.add(convertToC);
-
-    add(btnPanel, BorderLayout.SOUTH);
-
-}
+//         var input = new JTextField( 10);
+//         tempPanel.add(input);
+//         setPreferredSize(new Dimension(600, 600));
+//         // pack();
 
 
-private void setupResultPanel(){
-    var resultPanel = new JPanel();
+//     }
+// private void setupButtonPanel(){
+//     var btnPanel = new JPanel();
+//     var convertToF = new JButton("Convert to F");   btnPanel.add(convertToF);
+// convertToF.addActionListener(e->{
+//     System.out.println("Convert to F button clicked");
+//     resultLabel.setText("Result: 212 F");
+// });
+//     var convertToC = new JButton("Convert to C");    btnPanel.add(convertToC);
 
-resultLabel = new JLabel("Result:");
-resultPanel.add(resultLabel);
-add(resultPanel, BorderLayout.CENTER);
+//     add(btnPanel, BorderLayout.SOUTH);
 
-}
+// }
+
+
+// private void setupResultPanel(){
+//     var resultPanel = new JPanel();
+
+// resultLabel = new JLabel("Result:");
+// resultPanel.add(resultLabel);
+// add(resultPanel, BorderLayout.CENTER);
+
+// }
 }
         
